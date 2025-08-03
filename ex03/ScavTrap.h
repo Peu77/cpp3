f293c8b09f444a2e559632001eaa8e61ff7e3302
Emil Ebert <emilebert@icloud.com>
2025-08-03 10:05:24 +0200

    ups.. add missing virtuals

--- ex03/ScavTrap.h
+++ ex03/ScavTrap.h
@@ -19,7 +19,7 @@ public:
     ScavTrap& operator=(const ScavTrap& other);
     ~ScavTrap();
 
-    void attack(const std::string& target);
+    virtual void attack(const std::string& target);
     void guardGate();
 };
 
